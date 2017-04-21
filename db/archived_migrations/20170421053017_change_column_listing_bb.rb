cbbc4a590e6cdd6ad81b3d1ca87c3bb5d88e5117
Nandita Arora <arora.naina@gmail.com>
2017-04-21 00:28:07 -0700

    changing column type to integer for columns bedrooms and bathrooms

--- db/archived_migrations/20170421053017_change_column_listing_bb.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnListingBb < ActiveRecord::Migration[5.0]
-  def change
-    change_column :listings, :bedrooms, :integer
-    change_column :listings, :bathrooms, :integer
-    add_column :listings, :user_id, :integer
-  end
-end
